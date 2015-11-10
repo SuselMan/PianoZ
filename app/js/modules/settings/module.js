6db6e2ba2639254bc7ba03f1767467c8e4672ff7
i.pavluhin <i.pavluhin@yandex.ru>
2015-11-10 09:18:01 +0300

    popup region and view for settings

--- app/js/modules/settings/module.js
+++ app/js/modules/settings/module.js
@@ -36,6 +36,7 @@ define(function (require) {
 
     Module.on('stop', function() {
         console.log("AAAA");
+        window.history.back();
         return this.controller && (this.controller.destroy());
 
     });
