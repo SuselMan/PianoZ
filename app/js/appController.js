e4b28d07e8196144c2c7946caae20239eec0fb54
Ivan <vololo100@gmail.com>
2015-12-01 04:24:55 +0300

    replace each tab with 2 spaces

--- app/js/appController.js
+++ app/js/appController.js
@@ -3,29 +3,29 @@
  */
 
 define(function(require) {
-	"use strict";
+  "use strict";
 
-	var App = require('app');
+  var App = require('app');
   var channelGlobal = require('backbone.radio').channel('global');
 
-	var appController = {
+  var appController = {
 
-		index: function() {
-		  channelGlobal.request('navigate', 'gameWindow', {trigger: true, replace: true});
-		},
+    index: function() {
+      channelGlobal.request('navigate', 'gameWindow', {trigger: true, replace: true});
+    },
 
-		gameWindow: function(id) {
-		  var module = require('modules/gameWindow/module');
+    gameWindow: function(id) {
+      var module = require('modules/gameWindow/module');
 
-		  App.activeModule && App.activeModule.stop();
-	    App.activeModule = module;
-	    App.activeModule.start({id: id});
+      App.activeModule && App.activeModule.stop();
+      App.activeModule = module;
+      App.activeModule.start({id: id});
 
-	    channelGlobal.request('select:nav:item', 'gameWindow');
-	    channelGlobal.request('set:body:class', 'gameWindow');
-	    channelGlobal.request('hide:app:loader');
-		}
-	};
+      channelGlobal.request('select:nav:item', 'gameWindow');
+      channelGlobal.request('set:body:class', 'gameWindow');
+      channelGlobal.request('hide:app:loader');
+    }
+  };
 
-	return appController;
+  return appController;
 });
