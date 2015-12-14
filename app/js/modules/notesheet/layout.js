17c9f737e845808797706750106d1f75a3a41e0d
Alexey Stytsenko <stytsenko.a@gmail.com>
2015-12-14 21:35:01 +0000

    Switch hard-coded notesheets when button pushed

--- app/js/modules/notesheet/layout.js
+++ app/js/modules/notesheet/layout.js
@@ -6,6 +6,19 @@ define(function(require) {
     var Model = require('./model');
     var View = require('./view');
 
+    var abctemp = "X:1\n" +
+        "T:Notes\n" +
+        "M:C\n" +
+        "L:1/4\n" +
+        "K:C\n";
+    var data = [
+        { id: 0, text: 'test1', notesheet: abctemp + "C, D, E, F,|]\n" },
+        { id: 1, text: 'test2', notesheet: abctemp + "G, A, B, C|]\n" },
+        { id: 2, text: 'test3', notesheet: abctemp + "D  E  F  G|]\n" },
+        { id: 3, text: 'test4', notesheet: abctemp + "A  B  c  d|]\n" },
+        { id: 4, text: 'test5', notesheet: abctemp + "e  f  g  a|]\n" }
+    ];
+
     var Layout = Marionette.LayoutView.extend({
         template: require('hbs!templates/notesheet/layout'),
 
@@ -13,10 +26,30 @@ define(function(require) {
             sheetsShow: '.js-sheets-show'
         },
 
-        onRender: function () {
-            this.sheetsShow.show(new View({model: new Model}));
-        }
+        ui: {
+            playBtn:'#play'
+        },
 
+        events: {
+            'click @ui.playBtn': 'showSheetsView'
+        },
+
+        initialize: function() {
+            this.model = new Model();
+        },
+
+        onRender: function () {
+            this.$('#sheets-select').select2({
+                data: data,
+                placeholder: 'Select music sheets to play',
+                // allowClear: true
+            });
+        },
+
+        showSheetsView: function() {
+            this.model.set('notesheet', this.$('#sheets-select').select2('data').notesheet);
+            this.sheetsShow.show(new View({model: this.model}));
+        }
     });
 
     return Layout;
--- app/js/modules/notesheet/model.js
+++ app/js/modules/notesheet/model.js
@@ -7,17 +7,12 @@ define(function(require) {
 
         defaults: function() {
             return {
-                title: "Sample abc-js notesheet",
-                notesheet: "X:1\n" +
-                           "T:Notes\n" +
-                           "M:C\n" +
-                           "L:1/4\n" +
-                           "K:C\n" +
-                           "C, D, E, F,|G, A, B, C|D E F G|A B c d|e f g a|b c' d' e'|f' g' a' b'|]\n"
-
+                title: "",
+                notesheet: ""
             };
         }
     });
 
     return Model;
+
 });
